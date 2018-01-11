3184a636d05a8c1eb47ebfe40e093ff74b555115
JinTak <jtak23@gmail.com>
2018-01-11 10:38:39 -0700

    apikey stuff

--- server.js
+++ server.js
@@ -23,17 +23,19 @@ app.get('/food', function(req, res){
 
     console.log(apiId);
     console.log(apiKey);
-    request.get({
-        url: "https://api.nutritionix.com/v1_1/search/" + req.query.food + "?results=0%3A3&fields=item_name,brand_name,nf_calories,nf_total_carbohydrate,nf_protein,nf_total_fat,nf_serving_size_qty=1&appId=" + apiId.apiId + "&appKey=" + apiKey.apiKey + ""
-    }, function(err, response, body){
-        if(!err && response.statusCode == 200){
-            // console.log(typeof(body));
-            let jsonBody = JSON.parse(body);
-            // console.log(jsonBody);
-            res.render('foodResults.ejs', { jsonBody });   
+    // request.get({
+    //     url: "https://api.nutritionix.com/v1_1/search/" + req.query.food + "?results=0%3A3&fields=item_name,brand_name,nf_calories,nf_total_carbohydrate,nf_protein,nf_total_fat,nf_serving_size_qty=1&appId=" + apiId.apiId + "&appKey=" + apiKey.apiKey + ""
+    // }, function(err, response, body){
+    //     if(!err && response.statusCode == 200){
+    //         // console.log(typeof(body));
+    //         let jsonBody = JSON.parse(body);
+    //         // console.log(jsonBody);
+    //         res.render('foodResults.ejs', { jsonBody });   
 
-        }
-    });
+    //     }
+    // });
+
+    res.send('food route working');
 });
 
 
