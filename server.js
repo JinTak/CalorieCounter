a6b24d206e70cc4709ac54bd28047799ced5f42a
JinTak <jtak23@gmail.com>
2018-01-11 13:36:42 -0700

    GOT api requests TO WORK

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/food', function(req, res){
     console.log("Food route was hit");
 
    request.get({
-        url: "https://api.nutritionix.com/v1_1/search/" + req.query.food + "?results=0%3A3&fields=item_name,brand_name,nf_calories,nf_total_carbohydrate,nf_protein,nf_total_fat,nf_serving_size_qty=1&appId=" + apiId.apiId + "&appKey=" + apiKey.apiKey + ""
+        url: "https://api.nutritionix.com/v1_1/search/" + req.query.food + "?results=0%3A3&fields=item_name,brand_name,nf_calories,nf_total_carbohydrate,nf_protein,nf_total_fat,nf_serving_size_qty=1&appId=" + apiId + "&appKey=" + apiKey + ""
     }, function(err, response, body){
         if(!err && response.statusCode == 200){
             // console.log(typeof(body));
